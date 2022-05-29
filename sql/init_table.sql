cc9f5db5e06e87e416b0296c543fe8d82d499c83
BinaryCat17 <smirnov.vladimir17@gmail.com>
2022-05-29 20:48:51 +0300

    fdsa

--- sql/init_table.sql
+++ sql/init_table.sql
@@ -6,11 +6,7 @@ CREATE TABLE pool (
     skill varchar(32) NOT NULL,
     attr varchar(32) NOT NULL,
     equip varchar(32) NOT NULL,
-    relig varchar(32) NOT NULL,
-    race varchar(32) NOT NULL,
-    relat varchar(32) NOT NULL,
     PRIMARY KEY (pool_id)
 );
 
-\copy pool (name, class, spec, skill, attr, equip, relig, race, relat) FROM 'data/shiza-attr.csv' DELIMITER ';' CSV;
-delete from pool where name='name' 
+\copy pool (name, class, spec, skill, attr, equip) FROM 'data/shiza.csv' DELIMITER ';' CSV;
--- sql/test.sql
+++ sql/test.sql
@@ -1,1 +1,17 @@
-select pool_id, name, class, spec, skill, attr, equip, relig, race, SUBSTRING(relat, 0, LENGTH(relat)) as relat, CASE WHEN relat LIKE '%+' THEN 1 ELSE 0 END AS relat_val from pool;
+select
+    pool_id,
+    name,
+    class,
+    spec,
+    skill,
+    attr,
+    equip,
+    relig,
+    race,
+    SUBSTRING(relat, 0, LENGTH(relat)) as relat,
+    CASE
+        WHEN relat LIKE '%+' THEN 1
+        ELSE 0
+    END AS relat_val
+from
+    pool;
